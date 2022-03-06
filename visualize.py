81cc1846be96b2c3d98bc65d094793e320cfe2b5
Hakan "John" Gunerli <33205097+hakancangunerli@users.noreply.github.com>
2022-03-06 18:56:04 -0500

    push these nuts

--- visualize.py
+++ visualize.py
@@ -2,15 +2,16 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
-def visualize(flight_df):
+def visualize(flight_df,x):
 
-    fig = px.scatter_geo(flight_df, lat="lat", lon="long", hover_name="callsign")
+    fig = px.scatter_geo(flight_df, lat="lat", lon="long", hover_name="callsign", color = "lat", color_continuous_scale=px.colors.sequential.RdBu)
     fig.update_geos(lataxis_showgrid=True, lonaxis_showgrid=True)
 
     fig.update_layout(height=300, width=600)
 
     fig.update_layout(
-        geo_scope='europe',
+        geo_scope=str(x),
+        title_text="Flights in " + str(x),
         autosize=True,
         width=1000,
         height=1000,
@@ -21,8 +22,9 @@ def visualize(flight_df):
             t=100,
             pad=4
         ),
-
     ),
 
-    fig.show()
+    
     fig.write_image("./fig.png")
+    
+    return fig
